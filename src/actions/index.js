49c825a06524457feefbd4799b1f2fab1a4c93dc
Daniel Kovac <dkovac@logitech.com>
2019-03-12 12:35:10 -0700

    Added middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,13 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
-export const fetchPosts = async () => {
+export const fetchPosts = () =>
+    async dispatch => {
+        const response = await jsonPlaceholder.get('/posts');
+
+        dispatch({ type: 'FETCH_POSTS', payload: response });
+    };
+
+export const fetchPosts1 = async () => {
     const response = await jsonPlaceholder.get('/posts');
 
     return {
